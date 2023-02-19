5feeb8ef08f59c5d74bfc311b9f66e37d5b2b526
DiegoD97 <diego.delgadom@edu.uah.es>
2023-02-19 20:23:26 +0100

    Bloque de función para leer un txt con todas las secuencias y bloque de función que elimina los resultados procedente del fichero de pruebas

--- main_v2.py
+++ main_v2.py
@@ -12,7 +12,40 @@ import os
 ########################################################################################################################
 # CAPTURA DE LOS ARGUMENTOS DE ENTRADA
 ########################################################################################################################
+file_pruebas = open(os.path.join("Resources", "lista_pruebas.txt"))
+lista_pruebas = []
+for row in file_pruebas:
+    aux_list = row.split("\n")[0].split(" ")
+    sequence_aux = []
+    dict_aux = {}
+    for ind_arr in range(0, len(aux_list), 5):
+        dict_aux = {
+            'Node_init': aux_list[ind_arr + 2],
+            'Node_dest': aux_list[ind_arr + 3],
+            'dist': float(aux_list[ind_arr + 4])
+        }
+        sequence_aux.append([aux_list[ind_arr], float(aux_list[ind_arr + 1]), dict_aux])
 
+    lista_pruebas.append(sequence_aux)
+
+########################################################################################################################
+# ELIMINACIÓN DE LOS FICHEROS DE RESULTADOS CREADOS PREVIOS A LA EJECUCIÓN DEL PROGRAMA
+########################################################################################################################
+
+# Eliminación del contenido de DiscretResults
+
+for files_dir in os.listdir(os.path.join("Results", "DiscretResults")):
+    os.remove(os.path.join("Results", "DiscretResults", files_dir))
+
+# Eliminación del contenido de ResultsTracking
+for files_dir in os.listdir(os.path.join("Results", "ResultsTracking")):
+    os.remove(os.path.join("Results", "ResultsTracking", files_dir))
+
+# Eliminación del contenido de la carpeta ResultsWeights
+for files_dir in os.listdir(os.path.join("Results", "ResultsWeights")):
+    os.remove(os.path.join("Results", "ResultsWeights", files_dir))
+
+"""
 print("Procesando fichero " + sys.argv[1] + " ...")
 with open(os.path.join("Resources", sys.argv[1])) as file_json:
     data_diccionario = json.load(file_json)
@@ -30,7 +63,7 @@ for departamento in data_diccionario:
             dict_detections_nodes.append({'Node_init': list_aux[2],
                                           'Node_dest': list_aux[3],
                                           'dist': float(list_aux[4])})
-
+"""
 ########################################################################################################################
 # PARTE DEL TRACKING
 ########################################################################################################################
@@ -66,93 +99,93 @@ MAP = Map()
 # PARTE DE LA 'DETECTION'
 ########################################################################################################################
 # Tracking and Evaluating for three sequences
-for sequence, id_seq in zip(sequences, range(len(sequences))):
+for value_list in lista_pruebas:
+    for sequence, id_seq in zip(value_list, range(len(value_list))):
+        if (id_seq+1) == 1:
+            print(sequence)
+            DET.tracking_sequence(sequence[0])
+            # Reestart some variable for next iteration
+            DET.list_results = []
+            DET.list_YOLO_Camera_results = []
+            # Object Detection Sequence
+            # object_detections = MT.data_filter_objects(sequence[0]+"_Results.txt")
+            object_detections = DET.final_results_clustering
+            node_detection = sequence[2]
+            # Eval the weight
+            EW.evaluate_location(1,
+                                 MAP.NODE_AN.LIST_NODES,
+                                 MAP.NODE_AN.LIST_NODES_EDGES,
+                                 node_detection,
+                                 object_detections,
+                                 sequence[1])
 
-    if (id_seq+1) == 1:
-        print(sequence)
-        DET.tracking_sequence(sequence[0])
-        # Reestart some variable for next iteration
-        DET.list_results = []
-        DET.list_YOLO_Camera_results = []
-        # Object Detection Sequence
-        # object_detections = MT.data_filter_objects(sequence[0]+"_Results.txt")
-        object_detections = DET.final_results_clustering
-        node_detection = dict_detections_nodes[id_seq]
-        # Eval the weight
-        EW.evaluate_location(1,
-                             MAP.NODE_AN.LIST_NODES,
-                             MAP.NODE_AN.LIST_NODES_EDGES,
-                             node_detection,
-                             object_detections,
-                             sequence[1])
+        elif (id_seq+1) == 2:
+            print(sequence)
+            DET.tracking_sequence(sequence[0])
+            # Reestart some variable for next iteration
+            DET.list_results = []
+            DET.list_YOLO_Camera_results = []
+            # Object Detection Sequence
+            # object_detections = MT.data_filter_objects(sequence[0]+"_Results.txt")
+            object_detections = DET.final_results_clustering
+            node_detection = sequence[2]
+            # Eval the weight
+            EW.evaluate_location(2,
+                                 MAP.NODE_AN.LIST_NODES,
+                                 MAP.NODE_AN.LIST_NODES_EDGES,
+                                 node_detection,
+                                 object_detections,
+                                 sequence[1])
 
-    elif (id_seq+1) == 2:
-        print(sequence)
-        DET.tracking_sequence(sequence[0])
-        # Reestart some variable for next iteration
-        DET.list_results = []
-        DET.list_YOLO_Camera_results = []
-        # Object Detection Sequence
-        # object_detections = MT.data_filter_objects(sequence[0]+"_Results.txt")
-        object_detections = DET.final_results_clustering
-        node_detection = dict_detections_nodes[id_seq]
-        # Eval the weight
-        EW.evaluate_location(2,
-                             MAP.NODE_AN.LIST_NODES,
-                             MAP.NODE_AN.LIST_NODES_EDGES,
-                             node_detection,
-                             object_detections,
-                             sequence[1])
+        elif (id_seq + 1) == 3:
+            print(sequence)
+            DET.tracking_sequence(sequence[0])
+            # Reestart some variable for next iteration
+            DET.list_results = []
+            DET.list_YOLO_Camera_results = []
+            # Object Detection Sequence
+            # object_detections = MT.data_filter_objects(sequence[0]+"_Results.txt")
+            object_detections = DET.final_results_clustering
+            node_detection = sequence[2]
+            # Eval the weight
+            EW.evaluate_location(3,
+                                 MAP.NODE_AN.LIST_NODES,
+                                 MAP.NODE_AN.LIST_NODES_EDGES,
+                                 node_detection,
+                                 object_detections,
+                                 sequence[1])
 
-    elif (id_seq + 1) == 3:
-        print(sequence)
-        DET.tracking_sequence(sequence[0])
-        # Reestart some variable for next iteration
-        DET.list_results = []
-        DET.list_YOLO_Camera_results = []
-        # Object Detection Sequence
-        # object_detections = MT.data_filter_objects(sequence[0]+"_Results.txt")
-        object_detections = DET.final_results_clustering
-        node_detection = dict_detections_nodes[id_seq]
-        # Eval the weight
-        EW.evaluate_location(3,
-                             MAP.NODE_AN.LIST_NODES,
-                             MAP.NODE_AN.LIST_NODES_EDGES,
-                             node_detection,
-                             object_detections,
-                             sequence[1])
+        if (id_seq + 1) > 3:
+            # In case to explore more than 3 edges
+            # Now the matrix_W1 will be the matrix_W2 from before iteration and the matrix W2 will be the matrix W3 from
+            # before iteration
+            EW.W_1 = copy.deepcopy(EW.W_2)
+            EW.W_2 = copy.deepcopy(EW.W_3)
+            # Now is calculated the new matrix W_12
+            # First restart the matrix W_12
+            EW.W_12 = np.zeros([64, 64, 64])
+            # In this case is necessary evaluate the combinations for two Edges
+            for i in range(EW.W_1.shape[0]):
+                for j in range(EW.W_1.shape[1]):
+                    for k in range(EW.W_1.shape[1]):
+                        EW.W_12[i][j][k] = EW.W_1[i][j] * EW.W_2[j][k]
 
-    if (id_seq + 1) > 3:
-        # In case to explore more than 3 edges
-        # Now the matrix_W1 will be the matrix_W2 from before iteration and the matrix W2 will be the matrix W3 from
-        # before iteration
-        EW.W_1 = copy.deepcopy(EW.W_2)
-        EW.W_2 = copy.deepcopy(EW.W_3)
-        # Now is calculated the new matrix W_12
-        # First restart the matrix W_12
-        EW.W_12 = np.zeros([64, 64, 64])
-        # In this case is necessary evaluate the combinations for two Edges
-        for i in range(EW.W_1.shape[0]):
-            for j in range(EW.W_1.shape[1]):
-                for k in range(EW.W_1.shape[1]):
-                    EW.W_12[i][j][k] = EW.W_1[i][j] * EW.W_2[j][k]
-
-        print(sequence)
-        DET.tracking_sequence(sequence[0])
-        # Reestart some variable for next iteration
-        DET.list_results = []
-        DET.list_YOLO_Camera_results = []
-        # Object Detection Sequence
-        # object_detections = MT.data_filter_objects(sequence[0]+"_Results.txt")
-        object_detections = DET.final_results_clustering
-        node_detection = dict_detections_nodes[id_seq]
-        # Eval the weight
-        EW.evaluate_location(3,
-                             MAP.NODE_AN.LIST_NODES,
-                             MAP.NODE_AN.LIST_NODES_EDGES,
-                             node_detection,
-                             object_detections,
-                             sequence[1])
+            print(sequence)
+            DET.tracking_sequence(sequence[0])
+            # Reestart some variable for next iteration
+            DET.list_results = []
+            DET.list_YOLO_Camera_results = []
+            # Object Detection Sequence
+            # object_detections = MT.data_filter_objects(sequence[0]+"_Results.txt")
+            object_detections = DET.final_results_clustering
+            node_detection = sequence[2]
+            # Eval the weight
+            EW.evaluate_location(3,
+                                 MAP.NODE_AN.LIST_NODES,
+                                 MAP.NODE_AN.LIST_NODES_EDGES,
+                                 node_detection,
+                                 object_detections,
+                                 sequence[1])
 
 ########################################################################################################################
 ########################################################################################################################
